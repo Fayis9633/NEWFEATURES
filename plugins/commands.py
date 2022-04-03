3461d54f69921f073b26b6101aaced4a53e8e80c
Fayis9633 <97590049+Fayis9633@users.noreply.github.com>
2022-04-03 12:20:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🔥 GROUP 🔥', url='https://t.me/cwfffhe')
+                InlineKeyboardButton('📺 GROUP 📺', url='https://t.me/cwfffhe')
             ],
             [
                 InlineKeyboardButton('🔰 FEATURES 🔰', url=f"https://t.me/{temp.U_NAME}?start=help")
@@ -44,8 +44,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔥 GROUP 🔥', url='https://t.me/cwfffhe'),
-            InlineKeyboardButton('🔥 CHANNEL 🔥', url='https://t.me/+hcFeLTutE1w4Nzc1')
+            InlineKeyboardButton('📺 GROUP 📺', url='https://t.me/cwfffhe'),
+            InlineKeyboardButton('🎬 CHANNEL 🎬', url='https://t.me/Cinema_Worldx')
             ],[      
             InlineKeyboardButton('🔰 FEATURES 🔰', callback_data='help'),
             InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')
@@ -91,8 +91,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔥 GROUP 🔥', url='https://t.me/cwfffhe'),
-            InlineKeyboardButton('🔥 CHANNEL 🔥', url='https://t.me/+hcFeLTutE1w4Nzc1')
+            InlineKeyboardButton('📺 GROUP 📺', url='https://t.me/cwfffhe'),
+            InlineKeyboardButton('🎬 CHANNEL 🎬', url='https://t.me/Cinema_Worldx')
             ],[      
             InlineKeyboardButton('🔰 FEATURES 🔰', callback_data='help'),
             InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')
